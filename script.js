8c1cec44bef2233898a0419cc0700119d8bff1ff
RishiSoni252004 <rishisoni2504@gmail.com>
2025-02-28 16:56:30 +0530

    script

---
+++ script.js
@@ -0,0 +1,10 @@
+function addNewWEField() {
+    // console.log("Adding New field");
+    let newNode = DocumentFragment.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add(weField);
+    newNode.setAttribute("rows", 3);
+
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+}
