7a14091683f7eb723fc57ba7330786d26df6b2fc
RishiSoni252004 <rishisoni2504@gmail.com>
2025-03-01 14:28:09 +0530

    add buttond

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function addNewWEField() {
     // console.log("Adding New field");
-    let newNode = DocumentFragment.createElement("textarea");
+    let newNode = document.createElement("textarea");
     newNode.classList.add("form-control");
-    newNode.classList.add(weField);
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
     newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder","Enter here");
 
     let weOb = document.getElementById("we");
     let weAddButtonOb = document.getElementById("weAddButton");
+
+    weOb.insertBefore(newNode , weAddButtonOb);
+} 
+
+function addNewAQField()
+{
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("aqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder","Enter here");
+
+    let weOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+
+    weOb.insertBefore(newNode , aqAddButtonOb);
+
 }
